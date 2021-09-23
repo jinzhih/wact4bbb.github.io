4e11a29b1e29af6e298a22ddae0a83bf613e71b3
jinzhih <jessie.houjinzhi@gmail.com>
2021-09-24 02:49:43 +1000

    deploy

---
+++ precache-manifest.10ae0b3fc3a29da85ac4b9eb8936b76f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38d27e9f3f3075237a6aacd76fc829e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b5e11f795e43c0b70e",
+    "url": "/static/css/main.088ad0db.chunk.css"
+  },
+  {
+    "revision": "3690bb8bb808c0fb6991",
+    "url": "/static/js/2.c55df30e.chunk.js"
+  },
+  {
+    "revision": "6e6988987fa7d4f07ac57ac09f0836ca",
+    "url": "/static/js/2.c55df30e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b5e11f795e43c0b70e",
+    "url": "/static/js/main.6c6c4439.chunk.js"
+  },
+  {
+    "revision": "2a3496e0349049e73fa1",
+    "url": "/static/js/runtime-main.9aef5679.js"
+  },
+  {
+    "revision": "ae54f1b8d95136c189d9a0ba889b92fe",
+    "url": "/static/media/Baloo2-Bold.ae54f1b8.ttf"
+  },
+  {
+    "revision": "e2c3271576c410d8b097ed9809cb6539",
+    "url": "/static/media/Raleway-VariableFont_wght.e2c32715.ttf"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/static/media/Roboto-Regular.f36638c2.ttf"
+  },
+  {
+    "revision": "be7271c61119d5d03c3eadb75d5ddc2b",
+    "url": "/static/media/card1.be7271c6.png"
+  },
+  {
+    "revision": "69188d76333fc056ec610c5261cef01e",
+    "url": "/static/media/email.69188d76.svg"
+  },
+  {
+    "revision": "58d48889c60699ff8938abc437ead8f0",
+    "url": "/static/media/facebook.58d48889.svg"
+  },
+  {
+    "revision": "ab7e7f00908d6432aa9c6f9ea19f8e09",
+    "url": "/static/media/ins.ab7e7f00.png"
+  },
+  {
+    "revision": "441890b3c0e7f8cea371edd6969a72e0",
+    "url": "/static/media/logo.441890b3.png"
+  },
+  {
+    "revision": "f32809824090342e6626287c7b98ad73",
+    "url": "/static/media/phone.f3280982.svg"
+  },
+  {
+    "revision": "9f9b01f24ac3877561638256f6f11fd9",
+    "url": "/static/media/send.9f9b01f2.svg"
+  },
+  {
+    "revision": "a5423b0d581d20b2f6c85267b0d97c02",
+    "url": "/static/media/smalltour.a5423b0d.jpg"
+  }
+]);
